e66863bd8aaf542347c279f19fabbe6a9fbdf919
Shagil Islam <shagilislam786@gmail.com>
2022-07-07 14:01:13 +0530

    First Commit

---
+++ scripts/deploy.js
@@ -0,0 +1,16 @@
+async function main() {
+    // We get the contract to deploy
+    const TodoList = await ethers.getContractFactory("TodoList");
+    const todoList = await TodoList.deploy();
+  
+    await todoList.deployed();
+  
+    console.log("TodoList deployed to:", todoList.address);
+  }
+  
+  main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
